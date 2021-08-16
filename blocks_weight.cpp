9635794968b7bff7908e3786095addd8402b2424
Shakhin <67441571+Gyerzix@users.noreply.github.com>
2021-08-16 16:44:30 +0300

    Add files via upload

---
+++ blocks_weight.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+	int R; cin >> R;
+	uint64_t sum = 0;
+	for (int i = 0; i < N; i++) {
+		int W; cin >> W;
+		int H; cin >> H;
+		int D; cin >> D;
+		uint64_t temp = static_cast<uint64_t>(W) * H * D;
+		sum += temp;
+	}
+	cout << sum * R << endl;
+	return 0;
+}
